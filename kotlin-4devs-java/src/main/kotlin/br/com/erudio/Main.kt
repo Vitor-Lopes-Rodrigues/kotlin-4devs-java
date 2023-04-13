816c927ff13c3dae14b1e373791e59514b06de90
Cenpros <gabriel.rezende@cenpros.com.br>
2023-04-13 09:29:04 -0300

    Criando o projeto

--- kotlin-4devs-java/src/main/kotlin/br/com/erudio/Main.kt
+++ kotlin-4devs-java/src/main/kotlin/br/com/erudio/Main.kt
@@ -2,5 +2,5 @@ package br.com.erudio
 
 fun main(args: Array<String>) {
     val name = if(args.isNotEmpty()) args[0] else "Kotlin World"
-    println("Hello, $name 😊!!!")
+    println("Hello, $name 😊!!!!")
 }
